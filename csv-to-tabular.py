5f05bf59a759ebaedfa02f6be291ca914f929efa
Gabriel <gabriel.pelouze@u-psud.fr>
2015-11-19 11:26:49 +0100

    parse in format with regex

--- csv-to-tabular.py
+++ csv-to-tabular.py
@@ -9,23 +9,25 @@ def read_csv(filename):
 
 class Tex_src:
     
-    tab_row_sep = "\\hline\n"
-    tab_row_end = "\t\\\\\n"
+    tab_col_sep = "\t& "
+    tab_row_sep = "\\hline"
+    tab_row_end = "\t\\\\"
 
     def __init__(self):
         pass
 
     def begin_tabular(self, out_format):
-        return "\\begin{tabular}{"+out_format+"}\n"
+        return "\\begin{tabular}{"+out_format+"}"
 
     def end_tabular(self):
-        return "\\end{tabular}\n"
-
-    def tabular_complete_row(self, row):
-        return row.rstrip('\t& ') + '\t\\\\\n'
+        return "\\end{tabular}"
 
     def tabular_row(self, cells):
-        return "\t& ".join(cells) + self_row_end
+        return self.tab_col_sep.join(cells) + self.tab_row_end
+
+    def text(self, text, *args):
+        """ Dummy function used in `process_in_format` """
+        return text
 
     def num(self, num):
         return "\\num{"+num+"}"
@@ -44,44 +46,42 @@ def process_in_format(format_row):
     format the following rows.
     """ 
     functions = {
-        "t": (lambda t, *args: t, 1),
-        "n": (Tex_src.num, 1),
-        "nu": (Tex_src.SI, 2),
-        "ne": (Tex_src.num_err, 2),
-        "neu": (Tex_src.SI_err, 3),
+        "t": (tex_src.text, 1),
+        "n": (tex_src.num, 1),
+        "nu": (tex_src.SI, 2),
+        "ne": (tex_src.num_err, 2),
+        "neu": (tex_src.SI_err, 3),
         }
-    format_row = list(format_row)
+    # in `functions`, simple pattern entries (n, nu, ne, neu) contain tuples (function,
+    # nargs), while tricky patterns entries (t-+) contain only functions. 
+    in_format_string = "".join((
+        re_in_line.match(row[0]).group(1),
+        *(cell.strip(' ') for cell in row[1:])
+        ))
     in_format = []
-    last = {
-        'n': -3,
-        'e': -3,
-        'u': -3,
-        }
-    # format_row = 
-    for i,f in enumerate(format_row):
+    for chunk in re_in_format.finditer(in_format_string):
         try:
-            last[f] = i
-        except IndexError:
-            pass
-        if f == "u":
-            if i-last["n"] == 1:
-                in_format.append('nu')
-            elif i-last["n"] == 2 and i-last["e"] == 1:
-                in_format.append('neu')
-        else:
-            if i-last["n"] == 2:
-                if i-last["e"] == 1:
-                    in_format.append('ne')
-                else:
-                    in_format.append('n')
-            else:
-                in_format.append(format_row[i-2])
+            in_format.append(functions[chunk.group(0)])
+        except KeyError:
+            in_format.append((functions[chunk.group(1)][0], len(chunk.group(2)) + 1))
     return in_format
-    # FIXME: this does not work!
     
-re_in = re.compile("%in:% ?(.*)")
-re_out = re.compile("%out:% ?(.*)")
+re_in_line = re.compile("%in:% ?(.*)")
+re_out_line = re.compile("%out:% ?(.*)")
 re_hline = re.compile("%hline%")
+re_in_format = re.compile(
+    "(?:(t)(-+)?)|(?:n(?!u|(?:eu?)))|(?:nu)|(?:ne(?!u))|(?:neu)")
+# (t[-]+)|(n)|(nu)|(ne)|(neu)
+# simple patterns (ie n, nu, ne, neu) are returned in group 0
+# for tricky patterns (ie t-+) group 1 contains the key for functions dict in
+# process_in_format and group 2 contains the dashes (nargs-1 of the function)
+
+# Rules:
+# First row *may* be a %out:% lllll (any tabular format comprehensible by TeX).
+# Default out format: left-aligned, with as many columns as in the 1st row.
+# Any row *may* be a *in:* format, where each column contains one of t, n,
+# nu, ne, neu or -.
+# All following rows (until next %in:%) must follow this formatting.
 
 if __name__ == '__main__':
 
@@ -89,21 +89,29 @@ if __name__ == '__main__':
 
     tex_data = []
     out_format = ""
+    tex_src = Tex_src()
     f = open(filename, newline='')
     csv_reader = csv.reader(f, delimiter=',', quotechar='"')
-    for row in csv_reader:
-        print(row)
-        if re_in.match(row[0]):
-            in_format = (
-                re_in.match(row[0]).group(1),
-                *(cell.strip(' ') for cell in row[1:])
-                )
-            print(in_format)
-            print(process_in_format(in_format))
-        elif re_out.match(row[0]):
-            out_format = re_out.match(row[0]).group(1)
-            print(out_format)
+    for row_num, row in enumerate(csv_reader):
+        print("==>", row)
+        if row_num == 0:
+            if re_in_line.match(row[0]):
+                in_format = process_in_format(row)
+            else:
+                in_format = len(row) * "l"
+        elif re_out_line.match(row[0]):
+            out_format = re_out_line.match(row[0]).group(1)
         elif re_hline.match(row[0]):
-            tex_data += [Tex_src.tab_row_sep]
+            tex_data += [tex_src.tab_row_sep]
+        else:
+            col_num = 0
+            tex_row = []
+            for (func, nargs) in in_format:
+                tex_row.append(func(*row[col_num:col_num+nargs]))
+                col_num += nargs
+            tex_data += [tex_src.tabular_row(tex_row)]
+
+    tex_data = "\n".join(tex_data)
+    print(tex_data)
 
     f.close()
